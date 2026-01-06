aa4598354562d3061bb11f454877bf98ee2bb607
GuilhermeBiscalchin <desenvolvendogame@gmail.com>
2026-01-06 09:18:55 -0300

    Atualização arquivos

--- leitor/leitor-backend/server.js
+++ leitor/leitor-backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(MONGO_URI)
+mongoose.connect("mongodb+srv://biscalchinguilherme_db_user:V7NI9YjZTWLJG2GR@cluster0.hgcnat6.mongodb.net/?appName=Cluster0")
     .then(() => console.log("MongoDB conectado"))
     .catch(err => console.error("Erro MongoDB:", err));
 
